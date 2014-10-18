ab89b3f1ed9134abd2cad2177db00a1e35a2ee60
Aleksandr Grinspun <schurix@MacBookPro-3.local>
2014-10-19 00:18:05 +0200

    Initial Commit

---
+++ module/FSMPIVideo/config/module.config.php
@@ -0,0 +1,83 @@
+<?php
+namespace FSMPIVideo;
+
+return array(
+	'controllers' => array(
+		'invokables' => array(
+			'FSMPIVideo\Controller\Index' => 'FSMPIVideo\Controller\IndexController'
+		),
+	),
+	
+	'router' => array(
+		'routes' => array(
+			'home' => array(
+				'type' => 'literal',
+				'options' => array(
+					'route' => '/',
+					'defaults' => array(
+						'controller' => 'FSMPIVideo\Controller\Index',
+						'action'     => 'index',
+					),
+				),
+			),
+		),
+	),
+	
+	
+    'service_manager' => array(
+        'abstract_factories' => array(
+            'Zend\Cache\Service\StorageCacheAbstractServiceFactory',
+            'Zend\Log\LoggerAbstractServiceFactory',
+        ),
+        'aliases' => array(
+            'translator' => 'MvcTranslator',
+        ),
+    ),
+	
+    'translator' => array(
+        'locale' => 'de_DE',
+        'translation_file_patterns' => array(
+            array(
+                'type'     => 'gettext',
+                'base_dir' => __DIR__ . '/../language',
+                'pattern'  => '%s.mo',
+            ),
+        ),
+    ),
+	
+    'view_manager' => array(
+        'display_not_found_reason' => true,
+        'display_exceptions'       => true,
+        'doctype'                  => 'HTML5',
+        'not_found_template'       => 'error/404',
+        'exception_template'       => 'error/index',
+        'template_map' => array(
+            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+            'fsmpivideo/index/index'  => __DIR__ . '/../view/fsmpi-video/index/index.phtml',
+            'error/404'               => __DIR__ . '/../view/error/404.phtml',
+            'error/index'             => __DIR__ . '/../view/error/index.phtml',
+        ),
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
+	
+	
+
+	'navigation' => array(
+		'default' => array(
+			array('label' => 'VIDEOS',   'route' => 'home'),
+			array('label' => 'FSMPI',    'route' => 'home'),
+			array('label' => 'VAMPIR',   'route' => 'home')
+		),
+	),
+	
+    // Placeholder for console routes
+    'console' => array(
+        'router' => array(
+            'routes' => array(
+            ),
+        ),
+    ),
+
+);
